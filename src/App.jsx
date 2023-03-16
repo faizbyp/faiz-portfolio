0e00393fce5b0a214dc4c21f790e023c7585cbd5
faizbyp <faizbyp@gmail.com>
2023-03-16 12:14:35 +0700

    setup eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Test</h1>
-    </>
-  )
+    <h1>Test</h1>
+  );
 }
 
-export default App
+export default App;
